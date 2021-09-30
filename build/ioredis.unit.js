ad9d53eac3f4fd70fb8149750d647bd6b4af38a4
alpertl <87487862+alpertl@users.noreply.github.com>
2021-09-30 16:35:07 +0200

    modern imports.

--- build/ioredis.unit.js
+++ build/ioredis.unit.js
@@ -1,6 +1,11 @@
-const Redis = require('ioredis');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const ioredis_1 = __importDefault(require("ioredis"));
 require('./index.unit')({
     name: 'with driver "ioredis"',
-    redis: new Redis(),
+    redis: new ioredis_1.default(),
 });
 //# sourceMappingURL=ioredis.unit.js.map
--- src/ioredis.unit.ts
+++ src/ioredis.unit.ts
@@ -1,5 +1,4 @@
-const Redis = require('ioredis');
-
+import Redis from 'ioredis';
 require('./index.unit')({
   name: 'with driver "ioredis"',
   redis: new Redis(),
